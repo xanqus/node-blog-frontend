237a8edfd078a0b9c7b809a5f4033d69077bd644
xanqus <sbk98031816@gmail.com>
2021-04-28 16:59:35 +0900

    finish user information

--- dev/blog/blog-frontend/src/pages/PostListPage.jsx
+++ dev/blog/blog-frontend/src/pages/PostListPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Button from '../components/common/Button';
+
+import HeaderContainer from './../components/common/HeaderContainer';
 
 const LoginPage = () => {
   return (
-    <div>
-      <Button>버튼</Button>
-    </div>
+    <>
+      <HeaderContainer />
+      <div>안녕하세요.</div>
+    </>
   );
 };
 
